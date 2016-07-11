60178132dea5dbaa08f246010a67c91ce20b0048
Padmaja R <padmaja.ramesh@accoliteindia.com>
2016-07-11 21:40:03 +0530

    commit1

---
+++ JunitTest/src/JunitTest/TestSampleTest.java
@@ -0,0 +1,14 @@
+package JunitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
