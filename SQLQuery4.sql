4ea3a48bbba0d0003f60df09a25f9233bf25e368
Arnika23 <arnikatitoria@yahoo.com>
2016-07-13 08:44:39 +0530

    Delete SQLQuery4.sql

--- SQLQuery4.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT Cust.CompanyName, SOH.SubTotal, SOH.TaxAmt, SOH.Freight
-FROM [Assignment4].[SalesLT].[Customer] AS Cust
-INNER JOIN [Assignment4].[SalesLT].[SalesOrderHeader] AS SOH
-ON SOH.CustomerID = Cust.CustomerID
-WHERE 
-(SOH.SubTotal+SOH.TaxAmt+SOH.Freight)> 100000
