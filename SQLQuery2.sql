3f3e0a51a70c79cc52ccc4cce5597ee798a81ac2
Arnika23 <arnikatitoria@yahoo.com>
2016-07-13 08:44:14 +0530

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,4 +0,0 @@
-SELECT OrderQty,Name,ListPrice
-  FROM [Assignment4].[SalesLT].[SalesOrderHeader],[Assignment4].[SalesLT].[SalesOrderDetail] ,
-                [Assignment4].[SalesLT].[Product]
-WHERE CustomerID=635
