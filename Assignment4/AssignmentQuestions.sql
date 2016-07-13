1929a15bb68c2c14e6ffdd517cc3ad3fc02e14f3
mohitdevda1 <mohit.devda@accoliteindia.com>
2016-07-13 22:17:09 +0530

    Add files via upload
    5-8 Answer

--- Assignment4/AssignmentQuestions.sql
+++ Assignment4/AssignmentQuestions.sql
@@ -17,3 +17,40 @@ where City = 'Dallas');
 select CompanyName from CustomerAW where CustomerID in( select CustomerID 
 from SalesOrderHeader Group By CustomerID Having (Sum(SubTotal)+Sum(TaxAmt)+Sum(Freight)>100000));
 
+--Question5--
+select * from SalesOrderDetail
+select * from ProductAW
+
+select SalesOrderID, UnitPrice from SalesOrderDetail where SalesOrderID 
+IN (select SalesOrderID from SalesOrderDetail 
+Group by(SalesOrderID) 
+Having count(SalesOrderID)=1);
+
+
+-- solution 2
+select DISTINCT SalesOrderID, Count(DISTINCT ProductCategoryID) from SalesOrderDetail sod Join ProductAW pa on sod.ProductID=pa.ProductID
+Group by SalesOrderID
+Order by SalesOrderID;
+
+
+--Question6--
+Select pa.Name , ca.CompanyName from CustomerAW ca Join SalesOrderHeader soh on ca.CustomerID=soh.CustomerID Join SalesOrderDetail sod 
+on soh.SalesOrderID=sod.SalesOrderID Join  ProductAW pa on sod.ProductID=pa.ProductID  Join  ProductModel pm 
+on pa.ProductModelID=pm.ProductModelID
+where pm.Name='Racing Socks';
+
+--Question7--
+select * from Address ad Join CustomerAddress cad on ad.AddressID=cad.AddressID Join SalesOrderHeader soh 
+on cad.CustomerID=soh.CustomerID Join SalesOrderDetail sod on soh.SalesOrderID=sod.SalesOrderID 
+Join ProductAW pa on sod.ProductID=pa.ProductID Join ProductCategory pc on pa.ProductCategoryID=pc.ProductCategoryID
+  where ad.City='London' and pc.Name='Cranksets';
+
+
+--Question8--
+select TotalOrderValue=Sum(soh.SubTotal) 
+from SalesOrderHeader soh
+ JOIN CustomerAddress cadd on soh.CustomerID=cadd.CustomerID JOIN Address ad 
+ on cadd.AddressID=ad.AddressID
+Group by ad.CountryRegion
+Order by Sum(soh.SubTotal) desc;   
+
