295cb4c665c9f2dd16849469da298181cb63cbc2
ravik523 <ravi.kalmodia@accoliteindia.com>
2016-07-13 21:04:25 +0530

    Add files via upload

---
+++ dbms_as2/createquery.sql
@@ -0,0 +1,163 @@
+
+USE d1;
+CREATE TABLE [dbo].[Address](
+	[AddressID] [int] NOT NULL,
+	[AddressLine1] [varchar](100) NULL,
+	[AddressLine2] [varchar](100) NULL,
+	[City] [varchar](100) NULL,
+	[StateProvince] [varchar](100) NULL,
+	[CountryRegion] [varchar](100) NULL,
+	[PostalCode] [varchar](50) NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_Address] PRIMARY KEY CLUSTERED 
+(
+	[AddressID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[CustomerAW](
+	[CustomerID] [int] NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[MiddleName] [varchar](50) NULL,
+	[LastName] [varchar](50) NULL,
+	[CompanyName] [varchar](50) NULL,
+	[EmailAddress] [varchar](100) NULL,
+	[NameStyle] [int] NULL,
+	[Title] [varchar](50) NULL,
+	[Suffix] [varchar](100) NULL,
+	[SalesPerson] [varchar](100) NULL,
+	[Phone] [varchar](50) NULL,
+	[PasswordHash] [varchar](100) NULL,
+	[PasswordSalt] [varchar](100) NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_CustomerAW] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[CustomerAddress](
+	[CustomerID] [int] NOT NULL,
+	[AddressID] [int] NULL,
+	[AddressType] [varchar](50) NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_CustomerAddress] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[SalesOrderHeader](
+	[SalesOrderID] [int] NOT NULL,
+	[RevisionNumber] [int] NULL,
+	[OrderDate] [datetime] NULL,
+	[CustomerID] [int] NULL,
+	[BillToAddressID] [int] NULL,
+	[ShipToAddressID] [int] NULL,
+	[ShipMethod] [varchar](50) NULL,
+	[SubTotal] [int] NULL,
+	[TaxAmt] [int] NULL,
+	[Freight] [int] NULL,
+	[DueDate] [datetime] NULL,
+	[ShipDate] [datetime] NULL,
+	[Status] [int] NULL,
+	[OnlineOrderFlag] [int] NULL,
+	[PurchaseOrderNumber] [varchar](100) NULL,
+	[AccountNumber] [varchar](100) NULL,
+	[CreditCardApprovalCode] [varchar](100) NULL,
+	[Comment] [varchar](200) NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_SalesOrderHeader] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+
+
+
+
+CREATE TABLE [dbo].[ProductModel](
+	[ProductModelID] [int] NOT NULL,
+	[Name] [varchar](max) NULL,
+	[CatalogDescription] [varchar](max) NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_ProductModel] PRIMARY KEY CLUSTERED 
+(
+	[ProductModelID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[ProductAW](
+	[ProductID] [int] NOT NULL,
+	[Name] [varchar](max) NULL,
+	[Color] [varchar](max) NULL,
+	[ListPrice] [int] NULL,
+	[Size] [int] NULL,
+	[Weight] [int] NULL,
+	[ProductModelID] [int] NULL,
+	[ProductCategoryID] [int] NULL,
+	[StandardCost] [int] NULL,
+	[SellStartDate] [datetime] NULL,
+	[SellEndDate] [datetime] NULL,
+	[DiscontinuedDate] [datetime] NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+	[ThumbNailPhoto] [image] NULL,
+	[ThumbnailPhotoFileName] [varchar](max) NULL,
+	[ProductNumber] [varchar](max) NULL,
+ CONSTRAINT [PK_ProductAW] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[SalesOrderDetail](
+	[SalesOrderID] [int] NOT NULL,
+	[SalesOrderDetailID] [int] NOT NULL,
+	[OrderQty] [int] NULL,
+	[ProductID] [int] NULL,
+	[UnitPrice] [int] NULL,
+	[UnitPriceDiscount] [int] NULL,
+	[rowguid] [varchar](100) NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_SalesOrderDetail_1] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderDetailID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+create table ProductCategory(
+	[ProductCategoryID][int], 
+	[ParentProductCategoryID][int], 
+	[Name]nvarchar(max) , 
+	[rowguid]nvarchar(max) , 
+	[ModifiedDate][datetime],
+);
+GO
+
+create table ProductModelProductDescription(
+	[ProductModelID][int], 
+	[ProductDescriptionID][int], 
+	[Culture]nvarchar(max) , 
+	[rowguid]nvarchar(max) , 
+	[ModifiedDate][datetime],
+);
+GO
+
+create table ProductDescription(
+	[ProductDescriptionID][int], 
+	[Description]varchar(max) ,  
+	[rowguid]varchar(max) ,  
+	[ModifiedDate][datetime],
+);
+GO
---
+++ dbms_as2/readquery.sql
@@ -0,0 +1,102 @@
+﻿use d1;
+
+--q1
+
+SELECT CompanyName,AddressType,AddressLine1,AddressLine2,StateProvince, CountryRegion,PostalCode
+  FROM CustomerAW JOIN CustomerAddress
+    ON (CustomerAW.CustomerID=CustomerAddress.CustomerID)
+                  JOIN Address
+    ON (CustomerAddress.AddressID=Address.AddressID)
+ WHERE CompanyName='Modular Cycle Systems'
+
+--q2
+
+SELECT ProductAW.ListPrice,ProductAW.Name, SalesOrderDetail.OrderQty
+FROM ProductAW INNER JOIN SalesOrderDetail 
+  ON (ProductAW.ProductID=SalesOrderDetail.ProductID) 
+              INNER JOIN  SalesOrderHeader 
+  ON (SalesOrderHeader.​SalesOrderID=SalesOrderDetail​.SalesOrderID)
+WHERE SalesOrderHeader.CustomerID=635;
+
+--q3
+
+SELECT CompanyName 
+FROM CustomerAW INNER JOIN CustomerAddress 
+  ON (CustomerAW.CustomerID=CustomerAddress.CustomerID) 
+              INNER JOIN  Address 
+  ON (CustomerAddress​.AddressID=Address.AddressID)
+WHERE Address.City='Dallas';
+
+--q4
+
+select CompanyName
+from CustomerAW 
+where CustomerID in(   select CustomerID
+                        from SalesOrderHeader 
+						Group By CustomerID 
+						Having (Sum(SubTotal)+Sum(TaxAmt)+Sum(Freight)>100000)
+					);   
+     
+--q5
+
+--SELECT SalesOrderID , UnitPrice
+--FROM SalesOrderDetail
+--WHERE OrderQty=1;
+
+Select SalesOrderDetail.SalesOrderID,SalesOrderDetail.UnitPrice 
+from SalesOrderDetail 
+where SalesOrderID in (
+			 select sod.SalesOrderID 
+			 from SalesOrderDetail as sod 
+					   inner join SalesOrderHeader as sh 
+			 on (sod.SalesOrderID = sh.SalesOrderID) 
+			 group by sod.SalesOrderID 
+			 having count(sod.SalesOrderID) = 1
+) ;
+
+select * from SalesOrderDetail where SalesOrderID IN (71776,
+71867,
+71917,
+71946)
+--q6
+
+SELECT ProductAW.Name,CustomerAW.CompanyName,ProductModel.Name
+FROM CustomerAW JOIN SalesOrderHeader
+ON (CustomerAW.CustomerID=SalesOrderHeader.CustomerID)
+                JOIN SalesOrderDetail
+ON (SalesOrderHeader.SalesOrderID=SalesOrderDetail.SalesOrderID)
+                JOIN ProductAW
+ON (SalesOrderDetail.ProductID=ProductAW.ProductID)
+                JOIN ProductModel
+ON (ProductAW.ProductModelID=ProductModel.ProductModelID)
+WHERE ProductModel.Name='Racing Socks'
+
+--q7
+SELECT s1.productID, a1.city
+FROM   ProductCategory p2 JOIN ProductAW p1 
+ON p1.ProductCategoryID = p2.ProductCategoryID 
+                          JOIN SalesOrderDetail s1
+ ON p1.ProductID = s1.ProductID  
+                          JOIN SalesOrderHeader s2 
+ ON s2.SalesOrderID = s1.SalesOrderID 
+                          JOIN CustomerAW c1 
+ON s2.CustomerID = c1.CustomerID
+                          JOIN CustomerAddress c2 
+ON c1.CustomerID = c2.CustomerID
+                          JOIN Address a1 
+ON c2.AddressID = a1.AddressID 
+where p2.Name='Cranksets'and a1.City='London';   
+
+select * from ProductCategory p1 JOIN ProductAW p2 ON p1.ProductCategoryID = p2.ProductCategoryID 
+where p1.Name='Cranksets'
+
+select * from SalesOrderDetail where ProductID in ('949','950','951')
+--q8
+
+SELECT Address.CountryRegion, sum(subtotal) AS NumberOfOrders 
+FROM Address
+JOIN SalesOrderHeader  
+ON Address.AddressID=SalesOrderHeader.ShipToAddressID
+GROUP BY Address.CountryRegion;
+
+        
