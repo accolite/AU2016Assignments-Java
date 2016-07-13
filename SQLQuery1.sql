45115eedbb681b20765b65a2e0b795812d89b17f
Arnika23 <arnikatitoria@yahoo.com>
2016-07-13 08:44:01 +0530

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-SELECT CompanyName,AddressType,AddressLine1,City,StateProvince,CountryRegion,PostalCode
-  FROM [Assignment4].[SalesLT].[Customer] JOIN [Assignment4].[SalesLT].[CustomerAddress]
-    ON ([Assignment4].[SalesLT].[Customer].[CustomerID]=[Assignment4].[SalesLT].[CustomerAddress].[CustomerID])
-                  JOIN [Assignment4].[SalesLT].[Address]
-    ON ([Assignment4].[SalesLT].[CustomerAddress].[AddressID]=[Assignment4].[SalesLT].[Address].[AddressID])
- WHERE CompanyName='Modular Cycle Systems'
-
-
