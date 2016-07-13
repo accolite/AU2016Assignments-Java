220a2a25948fe661dd8e88af86d906dec97642c4
Arnika23 <arnikatitoria@yahoo.com>
2016-07-13 08:44:27 +0530

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT CompanyName
-FROM   [Assignment4].[SalesLT].[Customer] Caw
-  JOIN [Assignment4].[SalesLT].[CustomerAddress] Ca ON Caw.CustomerID = Ca.CustomerID
-  JOIN [Assignment4].[SalesLT].[Address] ADR ON Ca.AddressID = ADR.AddressID
-WHERE  City = 'Dallas';
