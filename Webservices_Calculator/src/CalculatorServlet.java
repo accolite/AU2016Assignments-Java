584d194b48bec3578017bf33842fc597003e08bb
anshika-accolite <anshika.agarwal@accoliteindia.com>
2016-07-19 19:41:41 +0530

    WebServices Calculator!

---
+++ Webservices_Calculator/src/CalculatorServlet.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.*;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class CalculatorServlet extends HttpServlet
+{
+        
+        public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException
+        {
+                
+                PrintWriter out = response.getWriter();
+                //out.println("hello");
+                String n1 = request.getParameter("no1");
+                String n2 = request.getParameter("no2");
+                String opt = request.getParameter("opt");                
+               if(opt.equals("p"))
+                        out.println(Integer.parseInt(n1) + Integer.parseInt(n2));
+                else if(opt.equals("m"))
+                       out.println(Integer.parseInt(n1) - Integer.parseInt(n2));
+                else if(opt.equals("mul"))
+                		out.println(Integer.parseInt(n1) * Integer.parseInt(n2));
+                else if(opt.equals("d")) 
+                {
+                		try
+               			{
+                			out.println(Integer.parseInt(n1) / Integer.parseInt(n2));	
+               			}
+               			catch(ArithmeticException e)
+               			{
+               				out.println("Invalid Choice");
+               			}
+                		
+                }
+               		
+              			
+        }        
+        public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException
+        {
+                doPost(request,response);
+        }
+        
+}
