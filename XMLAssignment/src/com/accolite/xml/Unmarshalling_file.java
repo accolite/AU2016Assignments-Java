02e0aca3984476d3ef33c1ea8a8cd2b98827f187
anshika-accolite <anshika.agarwal@accoliteindia.com>
2016-07-19 19:37:43 +0530

    Marshalling and Unmarshalling!!

---
+++ XMLAssignment/src/com/accolite/xml/Unmarshalling_file.java
@@ -0,0 +1,35 @@
+package com.accolite.xml;
+import java.io.File;
+ import javax.xml.bind.JAXBContext;
+ import javax.xml.bind.JAXBException;
+ import javax.xml.bind.Unmarshaller;
+ 
+public class Unmarshalling_file 
+{
+
+ public Unmarshalling_file() {
+  // TODO Auto-generated constructor stub
+ }
+  public static void main(String[] args) {
+
+   try {
+
+   File file = new File("D:\\NewFile.xml");
+   JAXBContext jaxbContext = JAXBContext.newInstance(Students.class);
+
+   Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+   Students student11 = (Students)jaxbUnmarshaller.unmarshal(file);
+   System.out.println("students");
+   for(StudentRecords std:student11.getStudents())
+   {
+   System.out.println(std.getEmail());
+   System.out.println(std.getFirstname());
+   System.out.println(std.getLastname());
+   System.out.println(std.getPhonenumber());
+   }
+    } catch (JAXBException e) {
+   e.printStackTrace();
+    }
+
+  }
+ }
