e27809b56039edd8c38893fb1c483454aea7e428
Lowkya123 <lowkya.v@accoliteindia.com>
2016-07-22 21:14:48 +0530

    Javascript Assignment

---
+++ JavaScript Assignement/Question3.js
@@ -0,0 +1,9 @@
+var list = readHugeList();
+var nextListItem = function() {
+var item = list.pop();
+if (item) {
+// process the list item...
+ setTimeout( nextListItem, 0 );
+}
+};
+nextListItem();
