7785588e3c021e2e9459fcdc5542b25950029c96
accolog10 <Lokesh.k@accoliteindia.com>
2016-07-24 23:16:29 +0530

    fifth commit

--- restandsoap/RestAndSoap/src/com/accolite/service/FB.java
+++ restandsoap/RestAndSoap/src/com/accolite/service/FB.java
@@ -1,6 +1,11 @@
+/****************************************************************************
+* Copyright (c) 2016 by Accolite.com. All rights reserved
+* Created date :: Jul 24, 2016
+*  @author :: Lokesh K
+* ***************************************************************************
+*/
 package com.accolite.service;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,78 +14,164 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FB.
+ */
 @Path("facebook")
 public class FB {
+	
+	/**
+	 * Gets the message.
+	 *
+	 * @param user the user
+	 * @return the message
+	 */
 	@GET
 	@Path("getMessage")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessage(@DefaultValue("") @QueryParam("user_name")String user){
-		return (new JSONHandler(user)).getMessage();
+	public String getMessage(@DefaultValue("") @QueryParam("user_name") String user) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null)
+			return jhandle.getMessage();
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Gets the all message.
+	 *
+	 * @param user the user
+	 * @return the all message
+	 */
 	@GET
 	@Path("getAllMessage")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getAllMessage(@DefaultValue("") @QueryParam("user_name")String user){
-		return new JSONHandler(user).getAllMessage();
+	public String getAllMessage(@DefaultValue("") @QueryParam("user_name") String user) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null)
+			return jhandle.getAllMessage();
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Gets the comment.
+	 *
+	 * @param user the user
+	 * @param messageID the message ID
+	 * @param commentID the comment ID
+	 * @return the comment
+	 */
 	@GET
 	@Path("getComment")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getComment(@DefaultValue("") @QueryParam("user_name")String user,
-			@DefaultValue("-1") @QueryParam("message_id")int messageID,
-			@DefaultValue("-1") @QueryParam("comment_id")int commentID){
-		return new JSONHandler(user).getComment(messageID, commentID);
+	public String getComment(@DefaultValue("") @QueryParam("user_name") String user,
+			@DefaultValue("-1") @QueryParam("message_id") int messageID,
+			@DefaultValue("-1") @QueryParam("comment_id") int commentID) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null)
+			return jhandle.getComment(messageID, commentID);
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Gets the all comment.
+	 *
+	 * @param user the user
+	 * @param messageID the message ID
+	 * @return the all comment
+	 */
 	@GET
 	@Path("getAllComment")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getAllComment(@DefaultValue("") @QueryParam("user_name")String user,
-			@DefaultValue("-1") @QueryParam("message_id")int messageID){
-		return new JSONHandler(user).getAllComment(messageID);
+	public String getAllComment(@DefaultValue("") @QueryParam("user_name") String user,
+			@DefaultValue("-1") @QueryParam("message_id") int messageID) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null)
+			return jhandle.getAllComment(messageID);
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Gets the likes.
+	 *
+	 * @param user the user
+	 * @param messageID the message ID
+	 * @return the likes
+	 */
 	@GET
 	@Path("getLikes")
 	@Produces(MediaType.TEXT_PLAIN)
-	public int getLikes(@DefaultValue("") @QueryParam("user_name")String user,
-			@DefaultValue("-1") @QueryParam("message_id")int messageID){
-		return new JSONHandler(user).getLikes(messageID);
+	public String getLikes(@DefaultValue("") @QueryParam("user_name") String user,
+			@DefaultValue("-1") @QueryParam("message_id") int messageID) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null)
+			return jhandle.getLikers(messageID);
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Adds the message.
+	 *
+	 * @param user the user
+	 * @param message the message
+	 * @return the string
+	 */
 	@POST
 	@Path("addMessage")
-	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String addMessage(@DefaultValue("") @QueryParam("user_name")String user,
-			@DefaultValue("") @QueryParam("message")String message){
-//		System.out.println(user+" "+message);
-		new JSONHandler(user).addMessage(message);
-		return getAllMessage(user);
+	public String addMessage(@DefaultValue("") @QueryParam("user_name") String user,
+			@DefaultValue("") @QueryParam("message") String message) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null){
+			jhandle.addMessage(message);
+			return getAllMessage(user);
+		}
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Adds the comment.
+	 *
+	 * @param user the user
+	 * @param comment the comment
+	 * @param messageID the message ID
+	 * @param whoCommented the who commented
+	 * @return the string
+	 */
 	@POST
 	@Path("addComment")
-	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String addComment(@DefaultValue("") @QueryParam("user_name")String user,
-			@DefaultValue("") @QueryParam("comment")String comment,
-			@DefaultValue("-1") @QueryParam("message_id")int messageID,
-			@DefaultValue("") @QueryParam("who_commented")String whoCommented){
-		new JSONHandler(user).addComment(messageID, comment, whoCommented);
-		return getAllMessage(user);
+	public String addComment(@DefaultValue("") @QueryParam("user_name") String user,
+			@DefaultValue("") @QueryParam("comment") String comment,
+			@DefaultValue("-1") @QueryParam("message_id") int messageID,
+			@DefaultValue("") @QueryParam("who_commented") String whoCommented) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null){
+			jhandle.addComment(messageID, comment, whoCommented);
+			return getAllMessage(user);
+		}
+		return "{\"error\":\"user not found\"}";
 	}
-	
+
+	/**
+	 * Adds the like.
+	 *
+	 * @param user the user
+	 * @param messageID the message ID
+	 * @param whoLiked the who liked
+	 * @return the string
+	 */
 	@POST
 	@Path("addLike")
-	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String addLike(@DefaultValue("") @QueryParam("user_name")String user,
-			@DefaultValue("-1") @QueryParam("message_id")int messageID){
-//		System.out.println(user+" "+messageID);
-		new JSONHandler(user).addLike(messageID);
-		return getAllMessage(user);
+	public String addLike(@DefaultValue("") @QueryParam("user_name") String user,
+			@DefaultValue("-1") @QueryParam("message_id") int messageID,
+			@DefaultValue("") @QueryParam("who_liked") String whoLiked) {
+		JSONHandler jhandle=new JSONHandler(user);
+		if(jhandle.user!=null){
+			jhandle.addLike(messageID,whoLiked);
+			return getAllMessage(user);
+		}
+		return "{\"error\":\"user not found\"}";
 	}
 }
--- restandsoap/RestAndSoap/src/com/accolite/service/JSONHandler.java
+++ restandsoap/RestAndSoap/src/com/accolite/service/JSONHandler.java
@@ -1,3 +1,9 @@
+/****************************************************************************
+* Copyright (c) 2016 by Accolite.com. All rights reserved
+* Created date :: Jul 24, 2016
+*  @author :: Lokesh K
+* ***************************************************************************
+*/
 package com.accolite.service;
 
 import java.io.File;
@@ -12,49 +18,70 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class JSONHandler.
+ */
 public class JSONHandler {
+	
+	/** The parser. */
 	JSONParser parser;
+	
+	/** The user array. Array of all  the users*/
 	JSONArray userArray;
+	
+	/** The user. current user who requires our service*/
 	JSONObject user;
+	
+	/** The user index. */
 	int userIndex;
+	
+	/** The file where data stored. */
 	File f;
-//	@SuppressWarnings("unchecked")
+
+	/**
+	 * Instantiates a new JSON handler.
+	 *
+	 * @param name the name
+	 */
+	// @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public JSONHandler(String name) {
 		parser = new JSONParser();
 		f = new File("FBdata.json");
-//		f.delete();
-		if(f.exists()){
+//		 f.delete();
+		if (f.exists()) {
 			try {
-				userIndex=0;
-				userArray=(JSONArray) parser.parse(new FileReader(f));
-				for(Object u:userArray){
-					JSONObject usr=(JSONObject)u;
-					if(name.equals((String)usr.get("name"))){
-						user=usr;
+				userIndex = 0;
+				userArray = (JSONArray) parser.parse(new FileReader(f));
+				for (Object u : userArray) {
+					JSONObject usr = (JSONObject) u;
+					if (name.equals((String) usr.get("name"))) {
+						user = usr;
 						break;
 					}
 					userIndex++;
 				}
-			}  catch (IOException e) {
-				// TODO Auto-generated catch block
+			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else{
+		} else {
 			try {
 				f.createNewFile();
 				userArray = new JSONArray();
-				user=new JSONObject();
+				//user1
+				user = new JSONObject();
 				user.put("name", "log10");
-				JSONArray userMessage=new JSONArray();
-				JSONObject message=new JSONObject();
+				JSONArray userMessage = new JSONArray();
+				JSONObject message = new JSONObject();
 				message.put("id", 0);
-				message.put("message","Happy Fun Friday");
+				message.put("message", "Happy Fun Friday");
 				message.put("like", 0);
-				JSONArray messageComment=new JSONArray();
-				JSONObject comment=new JSONObject();
+				message.put("likers", new JSONArray());
+				JSONArray messageComment = new JSONArray();
+				JSONObject comment = new JSONObject();
 				comment.put("id", 0);
 				comment.put("name", "jegan");
 				comment.put("comment", "boring pa!");
@@ -63,7 +90,28 @@ public class JSONHandler {
 				userMessage.add(message);
 				user.put("message", userMessage);
 				userArray.add(user);
-				FileWriter fw=new FileWriter(f);
+				//user2
+				user = new JSONObject();
+				user.put("name", "jegan");
+				userMessage = new JSONArray();
+				message = new JSONObject();
+				message.put("id", 0);
+				message.put("message", "Ethan peruda ithaiye kepinga");
+				message.put("like", 0);
+				message.put("likers", new JSONArray());
+				messageComment = new JSONArray();
+				message.put("comment", messageComment);
+				userMessage.add(message);
+				user.put("message", userMessage);
+				userArray.add(user);
+				//user3
+				user = new JSONObject();
+				user.put("name", "srithar");
+				userMessage = new JSONArray();
+				user.put("message", userMessage);
+				userArray.add(user);
+				//writing json into file
+				FileWriter fw = new FileWriter(f);
 				userArray.writeJSONString(fw);
 				fw.close();
 			} catch (IOException e) {
@@ -71,37 +119,64 @@ public class JSONHandler {
 			}
 		}
 	}
-	public void addMessage(String message){
-		JSONObject msg=new JSONObject();
-		JSONArray userMsg=(JSONArray)user.get("message");
-		msg.put("id",""+userMsg.size()+1);
+
+	/**
+	 * Adds the message.
+	 *
+	 * @param message the message
+	 */
+	@SuppressWarnings("unchecked")
+	public void addMessage(String message) {
+		JSONObject msg = new JSONObject();
+		JSONArray userMsg = (JSONArray) user.get("message");
+		msg.put("id", "" + userMsg.size() + 1);
 		msg.put("message", message);
 		msg.put("like", 0);
-		msg.put("comment",new JSONArray());
+		msg.put("likers", new JSONArray());
+		msg.put("comment", new JSONArray());
 		userMsg.add(msg);
 		user.put("message", userMsg);
 		userArray.set(userIndex, user);
 		try {
-			FileWriter fw=new FileWriter(f);
+			FileWriter fw = new FileWriter(f);
 			userArray.writeJSONString(fw);
 			fw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public String getMessage(){
-		JSONArray userMsg=(JSONArray)user.get("message");
-//		System.out.println(userMsg+"\n"+userMsg.toJSONString());
+
+	/**
+	 * Gets the message.
+	 *
+	 * @return the message
+	 */
+	public String getMessage() {
+		JSONArray userMsg = (JSONArray) user.get("message");
 		return userMsg.toJSONString();
 	}
-	public String getAllMessage(){
+
+	/**
+	 * Gets all user's message.
+	 *
+	 * @return the all message
+	 */
+	public String getAllMessage() {
 		return userArray.toJSONString();
 	}
+
+	/**
+	 * Adds the comment.
+	 *
+	 * @param messageID the message ID
+	 * @param comment the comment
+	 * @param name the name
+	 */
+	@SuppressWarnings("unchecked")
 	public void addComment(int messageID, String comment, String name) {
 		JSONArray userMsg = (JSONArray) user.get("message");
 		JSONObject message = (JSONObject) userMsg.get(messageID);
-		if(message==null)
+		if (message == null)
 			return;
 		JSONArray comments = (JSONArray) message.get("comment");
 		JSONObject cmnt = new JSONObject();
@@ -114,82 +189,95 @@ public class JSONHandler {
 		user.put("message", userMsg);
 		userArray.set(userIndex, user);
 		try {
-			FileWriter fw=new FileWriter(f);
+			FileWriter fw = new FileWriter(f);
 			userArray.writeJSONString(fw);
 			fw.close();
 			return;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public String getComment(int messageID, int commentID){
-		JSONArray userMsg=(JSONArray)user.get("message");
-		JSONObject message=(JSONObject)userMsg.get(messageID);
-		if(message==null)
+
+	/**
+	 * Gets the comment.
+	 *
+	 * @param messageID the message ID
+	 * @param commentID the comment ID
+	 * @return the comment
+	 */
+	public String getComment(int messageID, int commentID) {
+		JSONArray userMsg = (JSONArray) user.get("message");
+		JSONObject message = (JSONObject) userMsg.get(messageID);
+		if (message == null)
 			return "";
-		JSONArray comments = (JSONArray)message.get("comment");
-		JSONObject comment=(JSONObject)comments.get(commentID);
-		if(comment!=null)
+		JSONArray comments = (JSONArray) message.get("comment");
+		JSONObject comment = (JSONObject) comments.get(commentID);
+		if (comment != null)
 			return comment.toJSONString();
 		return "";
 	}
-	public String getAllComment(int messageID){
-		JSONArray userMsg=(JSONArray)user.get("message");
-		JSONObject message=(JSONObject)userMsg.get(messageID);
-		if(message==null)
+
+	/**
+	 * Gets the all comment.
+	 *
+	 * @param messageID the message ID
+	 * @return the all comment
+	 */
+	public String getAllComment(int messageID) {
+		JSONArray userMsg = (JSONArray) user.get("message");
+		JSONObject message = (JSONObject) userMsg.get(messageID);
+		if (message == null)
 			return "";
-		JSONArray comments = (JSONArray)message.get("comment");
+		JSONArray comments = (JSONArray) message.get("comment");
 		return comments.toJSONString();
 	}
-	public void addLike(int messageID){
-//		System.out.println(messageID+" "+user);
-		JSONArray userMsg=(JSONArray)user.get("message");
-		JSONObject message=(JSONObject)userMsg.get(messageID);
-		if(message==null)
+
+	/**
+	 * Adds the like.
+	 *
+	 * @param messageID the message ID
+	 * @param whoLiked the who liked
+	 */
+	@SuppressWarnings("unchecked")
+	public void addLike(int messageID,String whoLiked) {
+		JSONArray userMsg = (JSONArray) user.get("message");
+		JSONObject message = (JSONObject) userMsg.get(messageID);
+		if (message == null)
 			return;
-		int likes=-1;
-		try{
-			likes=((Integer)message.get("like")).intValue();
-		}catch(ClassCastException e){
-			likes=((Long)message.get("like")).intValue();
+		int likes = -1;
+		try {
+			likes = ((Integer) message.get("like")).intValue();
+		} catch (ClassCastException e) {
+			likes = ((Long) message.get("like")).intValue();
 		}
-//		System.out.println(likes+" "+message.get("like"));
-		message.put("like", likes+1);
-//		System.out.println(message.get("like"));
+		message.put("like", likes + 1);
+		JSONArray likers=(JSONArray)message.get("likers");
+		likers.add(whoLiked);
+		message.put("likers", likers);
 		userMsg.set(messageID, message);
 		user.put("message", userMsg);
 		userArray.set(userIndex, user);
 		try {
-			FileWriter fw=new FileWriter(f);
+			FileWriter fw = new FileWriter(f);
 			userArray.writeJSONString(fw);
 			fw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		System.out.println(new JSONHandler("log10").getLikes(messageID));
 	}
-	public int getLikes(int messageID){
-		JSONArray userMsg=(JSONArray)user.get("message");
-		JSONObject message=(JSONObject)userMsg.get(messageID);
-		if(message==null)
-			return -1;
-		try{
-			return ((Integer)message.get("like")).intValue();
-		}catch(ClassCastException e){
-			return ((Long)message.get("like")).intValue();
-		}
+
+	/**
+	 * Gets the likers.
+	 *
+	 * @param messageID the message ID
+	 * @return the likers
+	 */
+	public String getLikers(int messageID) {
+		JSONArray userMsg = (JSONArray) user.get("message");
+		JSONObject message = (JSONObject) userMsg.get(messageID);
+		if (message == null)
+			return "";
+		JSONArray likers = (JSONArray) message.get("likers");
+		return likers.toJSONString();
 	}
-//	public static void main(String[]args){
-//		JSONHandler jh=new JSONHandler("log10");
-//		System.out.println(jh.getMessage());
-//		jh.addMessage("Happy Week end");
-//		System.out.println(jh.getMessage());
-//		jh.addLike(1);
-//		System.out.println(jh.getLikes(1));
-//		jh.addComment(1, "week end sollave illa", "jegan");
-//		System.out.println(jh.getAllComment(1));
-//		System.out.println(jh.getComment(1, 0));
-//	}
 }
