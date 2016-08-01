21298ca9554aabe4e4b6602a4be856ba6903e017
Saumyadeep <saumyadeep.tarafdar@accoliteindia.com>
2016-08-01 23:53:48 +0530

    Business Unit Related API implemented

--- ProMa/src/com/au/proma/controller/BuController.java
+++ ProMa/src/com/au/proma/controller/BuController.java
@@ -1,10 +1,37 @@
 package com.au.proma.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.au.proma.model.BU;
+import com.au.proma.service.BuService;
 
 @Controller
 @RequestMapping("/bus")
 public class BuController {
+	
+	@Autowired
+	private BuService buService;
+	
+	@RequestMapping(method=RequestMethod.GET, produces="application/json")
+	@ResponseBody
+	public List<BU> getAllBU(){
+		return buService.getAllBU();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, produces="application/json",consumes="application/json")
+	@ResponseBody
+	public Integer addBU(@RequestBody BU bu){
+		return buService.addBU(bu);
+	}
+	
+	
+	
 
 }
--- ProMa/src/com/au/proma/dao/BuDao.java
+++ ProMa/src/com/au/proma/dao/BuDao.java
@@ -2,12 +2,14 @@ package com.au.proma.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.au.proma.model.BU;
 
 @Repository
 public class BuDao {
@@ -20,6 +22,25 @@ public class BuDao {
 
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	public List<BU> getAllBU(){
+		String sql = "select * from dbo.Bu";
+		return jdbcTemplate.query(sql, new RowMapper<BU>(){
+
+			@Override
+			public BU mapRow(ResultSet rs, int arg1) throws SQLException {
+				BU bu = new BU();
+				bu.setBuid(rs.getInt("buid"));
+				bu.setBuname(rs.getString("buname"));
+				return bu;
+			}
+		});
+	}
+	
+	public int addBU(BU bu){
+		String sql = "insert into dbo.Bu(buname) values('" + bu.getBuname() +"')";
+		return jdbcTemplate.update(sql);
 	}
 	
 }
