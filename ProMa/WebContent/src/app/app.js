5ee635ffffe6ed8019023212e2c6fbaa70ec27b5
Saumyadeep <saumyadeep.tarafdar@accoliteindia.com>
2016-08-03 01:39:25 +0530

    Integration With Angular done. Pages template created.

--- ProMa/WebContent/src/app/app.js
+++ ProMa/WebContent/src/app/app.js
@@ -1,73 +1,42 @@
 /**
-* library Module
+* app Module
 *
 * Description
 */
-angular.module('library', [
-	'books',
-	'ui.router',
+angular.module('app', [
+	'ngRoute',
+	'mgcrea.ngStrap'
 	])
-
-// angular.module('hrmsplus', [])
-// 	.controller('parentController', ['$scope','$rootScope', function($scope,$rootScope){
-// 		//$rootScope.controllerName= 'rootScope';	
-// 		$scope.controllerName = "ParentController";
-// 		$scope.contact = "parent";
-// 		$scope.destroyVar = function(){
-// 			delete $scope.contact;
-// 		}
-// 	}])
-// 	.controller('AppController', ['$scope', function($scope){
-		
-// 		//$scope.controllerName = "Child Controller";
-// 		$scope.destroyVar = function(){
-// 			delete $scope.contact;
-// 		}
-		
-// 	}])
-// 	.controller('AppController2',['$scope',function($scope){
-// 	//	$scope.controllerName = "Child Controller 2";
-// 		$scope.destroyVar= function(){
-// 			delete $scope.contact;
-// 		}
-// 	}])
-// 	.directive('hoverClass', ['$parse', function($parse){
-// 		// Runs during compile
-// 		return {
-// 			// name: '',
-// 			// priority: 1,
-// 			// terminal: true,
-// 			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-// 			// controller: function($scope, $element, $attrs, $transclude) {},
-// 			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-// 			restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-// 			// template: '',
-// 			// templateUrl: '',
-// 			// replace: true,
-// 			// transclude: true,
-// 			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-// 			link: function($scope, iElm, iAttrs, controller) {
-				
-// 				iElm.bind('mouseenter', function(e) {
-// 		        	iElm.addClass(iAttrs.hoverClass);
-// 		      	});
-
-// 				iElm.bind('mouseleave', function(e) {
-// 		        	iElm.removeClass(iAttrs.hoverClass);
-// 		      	});
-
-// 			}
-// 		};
-// 	}]);
-
-// var demoApp = angular.module('demoApp', []);
-
-// demoApp.controller('SimpleController', function ($scope) {
-//     $scope.customers = [
-//        { name: 'Dave Jones', city: 'Phoenix' },
-//        { name: 'Jamie Riley', city: 'Atlanta' },
-//        { name: 'Heedy Wahlin', city: 'Chandler' },
-//        { name: 'Thomas Winter', city: 'Seattle' }
-//     ];
-
-// });
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/',
+			{
+				controller:'overviewController',
+				templateUrl:'src/views/overview.html'
+			})
+			.when('/bu/:buid',
+			{
+				controller:'individualController',
+				templateUrl:'src/views/individual.html'
+			})
+			.otherwise({redirectTo: '/'});
+	})
+	.controller('overviewController', function($scope,$http){
+		var url = "rest/projects/status";
+		var promise = $http.get(url);
+		promise.then(function(response){
+			$scope.projects = response.data;
+		})		
+		$scope.modal = {
+		  "title": "Title",
+		  "content": "Hello Modal<br />This is a multiline message!"
+		};
+	})
+	.controller('individualController', function($scope,$http,$routeParams){
+		var url = "rest/projects/bus/" + $routeParams.buid;
+		var promise = $http.get(url);
+		promise.then(function(response){
+			$scope.projects = response.data;
+		})		
+		$scope.buid = $routeParams.buid
+	})
