606311d0ab73073575e9e00b7ba8eba48e9ee328
rarajeshks <rajesh.r@accoliteindia.com>
2016-08-04 15:59:48 +0530

    Clean Slate

--- AdPortal/src/com/au/adportal/util/Role.java
+++
@@ -1,8 +0,0 @@
-package com.au.adportal.util;
-
-public class Role {
-
-	public static final int USER = 1;
-	public static final int ADMIN = 2;
-	public static final int BLACKLISTED = 3;
-}
--- AdPortal/src/com/au/adportal/util/Status.java
+++
@@ -1,8 +0,0 @@
-package com.au.adportal.util;
-
-public class Status {
-
-	public static final int NEW = 1;
-	public static final int SOLD = 2;
-	public static final int DELETED = 3;
-}
